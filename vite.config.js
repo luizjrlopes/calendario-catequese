de3cf5c6b35c19f6c1fe8d71b8b470c4f9921024
luizjunior.lopes <luizjunior.lopes@gmail.com>
2025-08-02 12:57:24 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/calendario-vite/", // isso só afeta build
+  base: "/calendario-catequese/",
   plugins: [react()],
 });
